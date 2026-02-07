3a4d4d6ec9d34a468258aa3f230597bcd03550ff
Dmitry-Mee <78473902+Dmitry-Mee@users.noreply.github.com>
2026-02-07 18:22:42 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Находим кнопку в документе по её ID
+const btn = document.getElementById('main-button');
+
+// Описываем функцию, которая сработает при клике
+btn.onclick = function () {
+    alert('Поздравляю! Твой первый скрипт работает!');
+};
