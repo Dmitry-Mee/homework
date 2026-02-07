d8b8edf57e99ff65de7267540403fc2e754c4131
Dmitry-Mee <78473902+Dmitry-Mee@users.noreply.github.com>
2026-02-07 19:32:54 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 ﻿const btn = document.getElementById('main-button');
 const textElement = document.getElementById('dynamic-text');
 const input = document.getElementById('user-input'); // Находим поле ввода
+const list = document.getElementById('todo-list');
 
 btn.onclick = function () {
     const userName = input.value; // Забираем текст из поля
@@ -8,12 +9,40 @@ btn.onclick = function () {
     if (userName === "") {
         textElement.textContent = "Пожалуйста, введи имя!";
         textElement.style.color = "red";
-    } else {
+        textElement.style.fontFamily = "";
+        textElement.style.fontWeight = "";
+        textElement.style.fontSize = "";
+        
+    }
+    else if (userName === "Admin") {
+        textElement.textContent = "Hi, Admin!";
+        textElement.style.fontFamily = "cursive";
+        textElement.style.fontWeight = "bold";
+        textElement.style.fontSize = "24px";
+        textElement.style.color = "gold";
+    }
+    else {
         textElement.textContent = "Привет, " + userName + "! Ты делаешь успехи в JS!";
         textElement.style.color = "black";
+        textElement.style.fontFamily = "";
+        textElement.style.fontWeight = "";
+        textElement.style.fontSize = "";
 
         // Меняем цвет кнопки, как и раньше
         const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
         btn.style.backgroundColor = randomColor;
+
+        // --- РАБОТА СО СПИСКОМ ---
+
+        // 1. Создаем новый тег li
+        const newEntry = document.createElement('li');
+
+        // 2. Пишем в него текст (имя пользователя)
+        newEntry.textContent = "Пользователь: " + userName;
+
+        // 3. Добавляем этот li внутрь нашего ul (списка)
+        list.appendChild(newEntry);
+
+        input.value = "";
     }
-};
+    }
