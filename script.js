44132c79aa5f55ac5b32058954f60837bd0c2c37
Dmitry-Mee <78473902+Dmitry-Mee@users.noreply.github.com>
2026-02-07 18:41:58 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-﻿// Находим кнопку в документе по её ID
-const btn = document.getElementById('main-button');
+﻿const btn = document.getElementById('main-button');
+const text = document.getElementById('dynamic-text');
 
-// Описываем функцию, которая сработает при клике
+const phrases = [
+    "Я уже умею работать с GitHub Pages!",
+    "Я победил кодировку UTF-8!",
+    "JavaScript — это сила.",
+    "Мой код становится лучше с каждым коммитом."
+];
+
 btn.onclick = function () {
-    alert('Поздравляю! Твой первый скрипт работает!');
+    // 1. Меняем текст
+    const randomIndex = Math.floor(Math.random() * phrases.length);
+    text.textContent = phrases[randomIndex];
+
+    // 2. Меняем цвет кнопки на случайный
+    const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    btn.style.backgroundColor = randomColor;
 };
